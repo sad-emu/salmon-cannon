13cc5faa9cbcb78708f53267596074bde3dd39db
sad-emu <sad-emu@goaway.cc>
2025-11-03 19:47:23 +0000

    Safer read

--- socks_funcs.go
+++ socks_funcs.go
@@ -9,11 +9,11 @@ import (
 
 // Helper function to read exact number of bytes
 func readExact(conn net.Conn, buf []byte, n int) (int, error) {
-	if conn == nil {
-		return 0, fmt.Errorf("nil connection")
-	}
-	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 	defer conn.SetReadDeadline(time.Time{}) // Clear deadline after read
+
+	if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		return 0, err
+	}
 
 	total := 0
 	for total < n {
